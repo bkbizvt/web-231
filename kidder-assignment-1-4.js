ae1e20fcbba0b4512b4bc46a72a2437116c9e084
brennan Kidder <bkidder@my365.bellevue.edu>
2020-05-30 19:58:30 -0400

    assignment 1.4 upload

---
+++ kidder-assignment-1-4.js
@@ -0,0 +1,24 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Brennan Kidder
+; Date: 30 May 2020
+; Description: Primitive Types script. Assignment 1.4
+;===========================================
+*/
+
+
+
+
+var firstName = "Brennan" // my first name
+var lastName = "Kidder" // my last name
+var age = "I am " + 26 + " years old." //added string to help define what the number 26 is on the output but left the age as a number in the code for potential mathmatical uses.
+var street = "123 Green Mountain Road" // guess what state I'm from?
+var city = "Green Mountains City" // We really do have very green mountains!
+var zipCode = "05111" /* I purposefully made this a string becuase the zip code, while be a numeric value, should never be used in a mathmatical function 
+;and only displayed exactly as it is. */
+
+console.log (firstName, lastName) //combined the firstName and lastName variables for a better formatted output.
+console.log (age)
+console.log (street)
+console.log (city, zipCode)
