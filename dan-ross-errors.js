e1b38ac7435774a814e3d1264abe7cc01e753274
brennan Kidder <bkidder@my365.bellevue.edu>
2020-05-30 19:04:31 -0400

    dan_ross_corrected_2

--- dan-ross-errors.js
+++ dan-ross-errors.js
@@ -1,4 +1,4 @@
-var standard = 9;
+var standard = 9; // changed var default which is reserved by JS to standard
 var discount = 2;
-var total = standard - discount;
-console.log("The total is " + total);
+var total = standard - discount; //changed var default to standard and changed the capital D in discount to lowercase
+console.log("The total is " + total); //changed console-log to console.log
