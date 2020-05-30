213ee1d4d5cb73e9d13f56eb3753a04f4a621a08
brennan Kidder <bkidder@my365.bellevue.edu>
2020-05-30 19:08:44 -0400

    dan_ross_corrected_with_attribution

--- dan-ross-errors.js
+++ dan-ross-errors.js
@@ -1,3 +1,16 @@
+/*
+============================================
+; Title: Assignment 1.1
+; Author: Dan Ross
+; Date: 30 May 2020
+; Modified By: Brennan Kidder
+; Description: Values and variables with
+; at least two errors
+;===========================================
+*/
+
+ 
+
 var standard = 9; // changed var default which is reserved by JS to standard
 var discount = 2;
 var total = standard - discount; //changed var default to standard and changed the capital D in discount to lowercase
