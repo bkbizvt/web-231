137391bdc12de61af3e2782237a58606e28fd1e1
brennan Kidder <bkidder@my365.bellevue.edu>
2020-05-31 17:33:44 -0400

    erica update

--- erica-perry-errors.js
+++ erica-perry-errors.js
@@ -12,7 +12,7 @@ Description: At least two errors
 var firstName = "Erica"; //Removed the parantheses around the string. Strings should be enclosed in just quotation marks. 
 var lastName = "Perry" ;
 var Myage = "25" ; /*while this isn't necessarily going to cause an error if properly defined in output, Myage should be defined all lowercase or as myAge. Age should probably 
-; be input as a number not a string. Again this isn't breaking the output but would a more standard format would be as a number. */
+; be input as a number not a string. Again this isn't breaking the output but a more standard format would be as a number. */
 
 
 //output
