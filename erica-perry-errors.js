9321cf675ec2455005d6484dfc5e2e83368d1a8d
brennan Kidder <bkidder@my365.bellevue.edu>
2020-05-31 17:31:43 -0400

    erica perry

---
+++ erica-perry-errors.js
@@ -0,0 +1,23 @@
+/*
+
+Title: Perry-assignment-1.1.js
+Author: Erica Perry
+Date:5/30/2020
+Modified by: Brennan Kidder
+Description: At least two errors
+*/
+
+
+
+var firstName = "Erica"; //Removed the parantheses around the string. Strings should be enclosed in just quotation marks. 
+var lastName = "Perry" ;
+var Myage = "25" ; /*while this isn't necessarily going to cause an error if properly defined in output, Myage should be defined all lowercase or as myAge. Age should probably 
+; be input as a number not a string. Again this isn't breaking the output but would a more standard format would be as a number. */
+
+
+//output
+console.log (firstName) //Removed spacing and = sign that was erroring the output
+console.log (lastName) //Removed spacing that was erroring the output
+console.log (Myage); //Removed spacing that was erroring the output, corrected variable name from age to Myage
+
+//end program
