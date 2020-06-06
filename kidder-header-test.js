439931d39af9b7319431deb69124255be848f677
brennan Kidder <bkidder@my365.bellevue.edu>
2020-06-06 09:13:36 -0400

    Week 2 header update

---
+++ kidder-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   6June2020
+; Modified By: Brennan Kidder
+; Description:
+;===========================================
+*/
+
+/*testing Kidder specific header code*/
+const header = require('./kidder-header.js');
+console.log(header.display("Brennan", "Kidder", "Week 2"));
